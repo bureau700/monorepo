ec5b277d9acd5d63d481c5b76f39b37dd91aa090
Kevin Manson <kev.manson@gmail.com>
2020-10-18 13:24:51 -0400

    Fix deployment PORT.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,6 +4,7 @@ import pingService from '@jison/ping-service';
 import userService from '@jison/user-service';
 
 // const activateSwagger = process.env.NODE_ENV === 'development';
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 start().catch((err) => console.error(err));
 
@@ -21,7 +22,7 @@ async function start() {
   fastify.register(userService);
 
   try {
-    await fastify.listen(3000);
+    await fastify.listen(PORT);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
