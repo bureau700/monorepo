75b4c43236ce3516f94d8cf42ca777b708a2edc9
Kevin Manson <kev.manson@gmail.com>
2020-10-18 23:00:21 -0400

    Fix server binding address.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,7 @@
 import * as Fastify from 'fastify';
 import fastifySwagger from 'fastify-swagger';
 import pingService from '@jison/ping-service';
-// import userService from '@jison/user-service';
+import userService from '@jison/user-service';
 
 // const activateSwagger = process.env.NODE_ENV === 'development';
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
@@ -15,26 +15,25 @@ async function start() {
     },
   });
 
-  // fastify.register(fastifySwagger, {
-  //   routePrefix: '/documentation',
-  //   exposeRoute: true,
-  // });
+  fastify.register(fastifySwagger, {
+    routePrefix: '/documentation',
+    exposeRoute: true,
+  });
 
   fastify.register(pingService);
-  // fastify.register(userService);
+  fastify.register(userService);
 
-  fastify.ready(() => {
-    console.log(fastify.printRoutes());
-  });
+  // fastify.ready(() => {
+  //   console.log(fastify.printRoutes());
+  // });
 
-  fastify.setErrorHandler((error, request, reply) => {
-    console.log(error);
-    reply.code(500).send(error.message);
-  });
+  // fastify.setErrorHandler((error, request, reply) => {
+  //   console.log(error);
+  //   reply.code(500).send(error.message);
+  // });
 
   try {
-    console.log(process.env);
-    await fastify.listen(PORT);
+    await fastify.listen(PORT, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
